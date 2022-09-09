e2280c88f6f2670be46e692f72c84c00555fc3ad
Britton <brittonwhit97@gmail.com>
2022-09-09 14:07:11 -0600

    Added events
    Added particle effects called by events

--- Test Non-URP/Assets/Assignment/Scripts/Food.cs
+++ Test Non-URP/Assets/Assignment/Scripts/Food.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Food : MonoBehaviour
 {
     Flock_Brain brain;
+    [SerializeField] private UnityEvent callParticle;
+    [SerializeField] private ParticleSystem fx;
+
     private void Awake()
     {
         brain = GameObject.Find("Flock_Brain").GetComponent<Flock_Brain>();
@@ -12,6 +16,16 @@ public class Food : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         brain.activeFoods.Remove(this);
+        callParticle.Invoke();
+        fx.Play();
+        StartCoroutine(PlayParticles());
+    }
+
+    IEnumerator PlayParticles()
+    {
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        this.gameObject.GetComponent<SphereCollider>().enabled = false;
+        yield return new WaitForSeconds(1.5f);
         Destroy(this.gameObject);
     }
 }
